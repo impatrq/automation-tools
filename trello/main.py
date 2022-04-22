3d6f3dfb11dc5c884ba00cd378e1e4836360ed15
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-04-22 15:47:35 -0300

    feat: create Kanban board

--- trello/main.py
+++ trello/main.py
@@ -18,5 +18,15 @@ def createTeam(year, course, group, name):
     description = "Espacio de trabajo asignado al equipo {}, de 72{}{}. Año {}".format(name, course, group, year)
     nameUpdate = name.lower().replace(' ', '_')
     name= "{}_72{}{}_{}".format(year, course, group, nameUpdate)
-    client.add_organization(organization_name, description, name)
+    return client.add_organization(organization_name, description, name)
+
+def createBoard(name, team):
+    return client.add_board(board_name=name, organization_id=team.id, permission_level="public", default_lists=False)
+
+def createKanban(team):
+    board = createBoard('Kanban', team)
+    board.add_list('Terminado')
+    board.add_list('Revisión')
+    board.add_list('En proceso')
+    board.add_list('Para hacer')
 
