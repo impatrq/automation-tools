5094a906b3a357a4a8f87e84bf33536f47bfc080
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-04-22 12:53:36 -0300

    feat: include trello client object

--- trello/main.py
+++ trello/main.py
@@ -1,7 +1,13 @@
 from dotenv import load_dotenv
+from trello import TrelloClient
 import os
 trelloPath = os.getcwd()
 load_dotenv(trelloPath + '/.env')
 
 apiKey = os.getenv('TRELLO_API_KEY')
 apiSecret = os.getenv('TRELLO_API_SECRET')
+
+client = TrelloClient(
+    api_key=apiKey,
+    api_secret=apiSecret
+)
