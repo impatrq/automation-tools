8b2f5640427cb9a2464de1cefa803e582e67b983
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-04-22 14:49:33 -0300

    feat: create new team

--- trello/main.py
+++ trello/main.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 from trello import TrelloClient
 import os
+
 trelloPath = os.getcwd()
 load_dotenv(trelloPath + '/.env')
 
@@ -11,3 +12,11 @@ client = TrelloClient(
     api_key=apiKey,
     api_secret=apiSecret
 )
+
+def createTeam(year, course, group, name):
+    organization_name = "{} 72{}{} {}".format(year, course, group, name)
+    description = "Espacio de trabajo asignado al equipo {}, de 72{}{}. Año {}".format(name, course, group, year)
+    nameUpdate = name.lower().replace(' ', '_')
+    name= "{}_72{}{}_{}".format(year, course, group, nameUpdate)
+    client.add_organization(organization_name, description, name)
+
