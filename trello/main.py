ffc6bba57c5000a4077055fab435b4f8d8576fde
Sergio Medina <63879459+sergiomedinaio@users.noreply.github.com>
2022-04-22 12:53:18 -0300

    feat: get trello secrets

--- trello/main.py
+++ trello/main.py
@@ -3,5 +3,5 @@ import os
 trelloPath = os.getcwd()
 load_dotenv(trelloPath + '/.env')
 
-apiKey = os.getenv('API_KEY')
-print(apiKey)
+apiKey = os.getenv('TRELLO_API_KEY')
+apiSecret = os.getenv('TRELLO_API_SECRET')
